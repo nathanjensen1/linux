c7467c9adaadbc9286501e26e765bcb6b685da79
Sarah Walker <sarah.walker@imgtec.com>
2022-10-12 13:38:15 +0100

    drm/imagination: Remove implicit sync support from job UAPI
    Signed-off-by: Sarah Walker <sarah.walker@imgtec.com>
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>

--- include/uapi/drm/pvr_drm.h
+++ include/uapi/drm/pvr_drm.h
@@ -813,29 +813,6 @@ struct drm_pvr_ioctl_vm_unmap_args {
 };
 
 /**
- * DOC: Flags for &struct drm_pvr_bo_ref.flags
- *
- * DRM_PVR_BO_REF_READ - This buffer object will be read by the device.
- *
- * DRM_PVR_BO_REF_WRITE - This buffer object will be written to by the device.
- */
-#define DRM_PVR_BO_REF_READ _BITUL(0)
-#define DRM_PVR_BO_REF_WRITE _BITUL(1)
-
-#define DRM_PVR_BO_REF_FLAGS_VALID_MASK (DRM_PVR_BO_REF_READ | DRM_PVR_BO_REF_WRITE)
-
-/**
- * struct drm_pvr_bo_ref - structure representing a DRM buffer object
- */
-struct drm_pvr_bo_ref {
-	/** @handle: DRM buffer object handle. */
-	__u32 handle;
-
-	/** @flags: Flags for this buffer object. Must be a combination of %DRM_PVR_BO_REF_*. */
-	__u32 flags;
-};
-
-/**
  * DOC: Flags for SUBMIT_JOB ioctl geometry command.
  *
  * Operations
@@ -973,23 +950,9 @@ struct drm_pvr_job_render_args {
 	__u64 in_syncobj_handles_frag;
 
 	/**
-	 * @bo_handles: [IN] Pointer to array of struct drm_pvr_bo_ref.
-	 *
-	 * This array must be &num_bo_handles entries large.
-	 */
-	__u64 bo_handles;
-
-	/**
 	 * @num_in_syncobj_handles_frag: [IN] Number of input syncobj handles for fragment job.
 	 */
 	__u32 num_in_syncobj_handles_frag;
-
-	/**
-	 * @num_bo_handles: [IN] Number of DRM Buffer Objects.
-	 *
-	 * This detemines the size of the array &bo_handles points to.
-	 */
-	__u32 num_bo_handles;
 
 	/**
 	 * @out_syncobj_geom: [OUT] drm_syncobj handle for geometry output fence
@@ -1078,20 +1041,6 @@ struct drm_pvr_job_compute_args {
 	__u32 ext_stream_len;
 
 	/**
-	 * @bo_handles: [IN] Pointer to array of struct drm_pvr_bo_ref.
-	 *
-	 * This array must be &num_bo_handles entries large.
-	 */
-	__u64 bo_handles;
-
-	/**
-	 * @num_bo_handles: [IN] Number of DRM Buffer Objects.
-	 *
-	 * This detemines the size of the array &bo_handles points to.
-	 */
-	__u32 num_bo_handles;
-
-	/**
 	 * @flags: [IN] Flags for command.
 	 */
 	__u32 flags;
@@ -1137,20 +1086,6 @@ struct drm_pvr_job_transfer_args {
 	 * @ext_stream_len: [IN] Length of transfer command extension stream, in bytes.
 	 */
 	__u32 ext_stream_len;
-
-	/**
-	 * @bo_handles: [IN] Pointer to array of struct drm_pvr_bo_ref.
-	 *
-	 * This array must be &num_bo_handles entries large.
-	 */
-	__u64 bo_handles;
-
-	/**
-	 * @num_bo_handles: [IN] Number of DRM Buffer Objects.
-	 *
-	 * This detemines the size of the array &bo_handles points to.
-	 */
-	__u32 num_bo_handles;
 
 	/**
 	 * @flags: [IN] Flags for command.
